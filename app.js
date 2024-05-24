9a73b5381019f02c9c544ef1a1c67ec8e9afaf37
Caleb <cjschaun@gmail.com>
2024-05-24 08:24:40 -0500

    blog update

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
 const path = require('path');
+const fs = require('fs-extra');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config();
 
+
+app.use(
+    cors({
+        origin: 'http://localhost:' + port,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    })
+)
+
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public', 'static', 'style')));
 app.use(express.static(path.join(__dirname, 'public', 'static', 'scripts')));
 app.use(express.static(path.join(__dirname, 'public', 'static', 'images')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Authentication
+function authenticate(req, res, next) {
+    if (req.session && req.session.authenticated) {
+        return next();
+    } else {
+        return res.redirect('/admin');
+    }
+}
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
@@ -17,14 +47,161 @@ app.get('/home', (req, res) => {
     res.redirect('/');
 });
 
-// app.get('/blog', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'blog.html'))
-// });
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blog.html'))
+});
+
+app.get('/blog-editor', authenticate, (req, res) => {
+    res.sendFile(path.join(__dirname, 'private', 'blog-editor.html'))
+})
+
+// Handle admin stuff
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'private', 'admin.html'))
+})
+
+app.post('/admin', (req, res) => {
+    let body = req.body;
+    console.log(body);
+    if (body.username == process.env.UNAME && body.password == process.env.PWD) {
+        req.session.authenticated = true;
+        res.send({success: true});
+        return;
+    }
+    res.send({success: false});
+})
+
+app.get('/our-story', (req, res) => {
+    // let bl = {
+    //     "title": "Sailing to Maryland",
+    //     "date": '01/10/2024',
+    //     "public": true,
+    //     'content': "As the sun rose over the horizon, casting a warm glow over the waters of Rhode Island, I couldn't help but feel a sense of excitement mixed with a hint of nervousness. Our family of five was about to embark on a sailing adventure from Rhode Island to Maryland, a journey that promised to be both thrilling and challenging.\nThe boat gently rocked as we boarded, the boys eagerly exploring every nook and cranny while our little girl, with her eyes wide with wonder, clung to her mother's hand. As we set sail, the wind filled the sails, propelling us forward into the vast expanse of the ocean.\nThe first few hours were pure bliss, the sun shining down on us as we sailed along, the salty sea air filling our lungs. The boys were full of energy, their laughter ringing out as they climbed the rigging and pretended to be pirates.\nBut as the day wore on and the sea grew rougher, things took a turn for the worse. The boys, who had been so full of life earlier, suddenly turned pale and clammy, their faces twisted in discomfort. Seasickness had struck, and it wasn't long before they were leaning over the side of the boat, emptying their stomachs into the churning waters below.\nMy wife and I did our best to comfort them, but there was little we could do to ease their suffering. Meanwhile, our little girl, unaffected by the motion of the ocean, chugged along happily by our side, her eyes wide with wonder at the sights and sounds of the sea.\nDespite the challenges, we pressed on, determined to make the most of our adventure. We sailed through the night, taking turns at the helm, the stars shining down on us like diamonds in the sky. And finally, after what seemed like an eternity, we arrived in Maryland, tired but exhilarated by our journey.\nAs we disembarked, the boys weak but smiling, our little girl skipping ahead, I couldn't help but feel a sense of pride. We had faced adversity head-on and emerged stronger for it. And as we made our way home, the boys already planning our next adventure, I knew that this would be a vacation we would never forget.",
+    // }
+
+    // addBlog(bl);
+
+    updatePublicDB();
+
+});
+
+app.get('/api/get-blog', (req, res) => {
+    if (req.query.hasOwnProperty('year')) {
+        fs.readJSON('public/static/scripts/blogs.json').then(pb => {
+            if (req.query.hasOwnProperty('month')) {
+                if (req.query.hasOwnProperty('day')) {
+                    if (req.query.hasOwnProperty('id')) {
+                        if (pb.hasOwnProperty(req.query['year']) && pb[req.query['year']].hasOwnProperty(req.query['month']) && pb[req.query['year']][req.query['month']].hasOwnProperty(req.query['day']) && pb[req.query['year']][req.query['month']][req.query['day']].length > parseInt(req.query['id']) && parseInt(req.query['id']) >= 0) {
+                            res.send([pb[req.query['year']][req.query['month']][req.query['day']][parseInt(req.query['id'])]]);
+                        } else {
+                            res.send([]);
+                        }
+                    } else {
+                        if (pb.hasOwnProperty(req.query['year']) && pb[req.query['year']].hasOwnProperty(req.query['month']) && pb[req.query['year']][req.query['month']].hasOwnProperty(req.query['day'])) {
+                            res.send(pb[req.query['year']][req.query['month']][req.query['day']]);
+                        } else {
+                            res.send([]);
+                            return;
+                        }
+                    }
+                } else {
+                    if (pb.hasOwnProperty(req.query['year']) && pb[req.query['year']].hasOwnProperty(req.query['month'])) {
+                        res.send(pb[req.query['year']][req.query['month']]);
+                    } else {
+                        res.send([]);
+                        return;
+                    }
+                }
+            } else {
+                if (pb.hasOwnProperty(req.query['year'])) {
+                    res.send(pb[req.query['year']]);
+                } else {
+                    res.send([]);
+                    return;
+                }
+            }
+        });
+    } else {
+        res.send([]);
+        return;
+    }
+});
 
 
 app.listen(port, '0.0.0.0', () => {
     console.log("Server is running at http://localhost:" + port);
 });
+
+
+function addBlog(bl, db_fp='db.json') {
+    let date_info = bl["date"].split('/');
+    if (date_info.length != 3) {
+        return "Invalid Date";
+    }
+
+    let month = date_info[0];
+    let day = date_info[1];
+    let year = date_info[2];
+
+    let j = fs.readJSON(db_fp).then(res => {
+        if (res.hasOwnProperty(year)) {
+            if (res[year].hasOwnProperty(month)) {
+                if (res[year][month].hasOwnProperty(day)) {
+                    for (let i in res[year][month][day]) {
+                        if (res[year][month][day][i]["title"] == bl["title"]) {
+                            console.log('Title Exists')
+                            return "Title Exists"
+                        }
+                    }
+                    res[year][month][day].push(bl);
+                } else {
+                    res[year][month][day] = [];
+                    res[year][month][day].push(bl);
+                }
+            } else {
+                res[year][month] = {};
+                res[year][month][day] = [];
+                res[year][month][day].push(bl);
+            }
+        } else {
+            res[year] = {};
+            res[year][month] = {};
+            res[year][month][day] = [];
+            res[year][month][day].push(bl);
+        }
+    
+        fs.writeJSON(db_fp, res);
+        
+        return null;
+    });
+
+    return "Something went wrong";
+}
+
+
+function updatePublicDB(db_fp='db.json') {
+    fs.readJSON(db_fp).then(db => {
+        pb = {}
+        for (let year in db) {
+            pb[year] = {};
+            for (let month in db[year]) {
+                pb[year][month] = {};
+                for (let day in db[year][month]) {
+                    pb[year][month][day] = [];
+                    for (let i in db[year][month][day]) {
+                        if (db[year][month][day][i]["public"]) {
+                            pb[year][month][day].push(db[year][month][day][i]);
+                        }
+                    }
+                }
+            }
+        }
+        fs.writeJSON(path.join(__dirname, 'public', 'static', 'scripts', 'blogs.json'), pb)
+        .catch(e => {
+            console.log('Something went wrong when updating public database:', e)
+        });
+    })
+}
 
 
 
--- public/static/scripts/blog.js
+++ public/static/scripts/blog.js
@@ -1,43 +1,3 @@
-
-function getSelectedYear() {
-    let e = document.querySelector('.years');
-    if (e != null) {
-        return e.querySelector('.arc.selected');
-    }
-    return null;
-}
-
-function selectYear(e) {
-    let y = getSelectedYear();
-    if (y != null) {
-        y.classList.remove('selected');
-    }
-    e.classList.add('selected');
-}
-
-
-blog_posts = [
-    {
-        title: 'Sailing to Maryland',
-        blog: 'Test Test Test',
-        image_path: 'static/blog_posts/1.jpg'
-    }
-]
-
-// years = [
-//     '2024',
-//     '2023',
-//     '2022',
-//     '2021'
-// ]
-
-years = []
-for (let i = 2030; i > 2022; i--) {
-    years.push(i.toString());
-}
-
-console.log(years);
-
 months = [
     'January',
     'February',
@@ -53,71 +13,180 @@ months = [
     'December',
 ]
 
+months_n = [
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12'
+]
 
-archive_HTML = document.querySelector('.archive');
-years_HTML = archive_HTML.querySelector('.years');
-months_HTML = archive_HTML.querySelector('.months');
+function goTime() {
+    let month = document.querySelector('select#month');
+    let year = document.querySelector('select#year');
 
-for (let i = 0; i < years.length; i++) {
-    let yearDiv = document.createElement('div');
-    yearDiv.innerHTML = years[i];
-    yearDiv.classList.add('arc');
-    if (i == 0) {
-        yearDiv.classList.add('selected');
-    }
-    yearDiv.addEventListener('click', function(event) {
-        selectYear(event.currentTarget);
-    });
-    years_HTML.appendChild(yearDiv);
-    if (i != years.length-1) {
-        let connecter = document.createElement('div');
-        connecter.classList.add('connecter');
-        years_HTML.appendChild(connecter);
+    let monthSelected = month.options[month.selectedIndex].value;
+    let yearSelected = year.options[year.selectedIndex].value;
+
+    if (monthSelected == 'All') {
+        location.href = '/blog?year=' + yearSelected;
+    } else {
+        location.href = '/blog?year=' + yearSelected + '&month=' + months_n[months.indexOf(monthSelected)];
     }
 }
 
-for (let i = 0; i < months.length; i++) {
-    let monthDiv = document.createElement('div');
-    monthDiv.innerHTML = months[i];
-    monthDiv.classList.add('montharc');
-    months_HTML.appendChild(monthDiv);
-    // if (i != months.length-1) {
-    //     let connecter = document.createElement('div');
-    //     connecter.classList.add('monthconnecter');
-    //     months_HTML.appendChild(connecter);
-    // }
-}
+function load() {
+    const urlParams = new URLSearchParams(window.location.search);
 
-// archive_HTML.style = 'height: ' + years.length * 125 + 'px';
+    let key_order = ["year", "month", "day"]
 
+    const year = urlParams.get('year');
+    if (year != null) {
 
-let url = window.location.href;
-let paramsStr = url.substring(url.indexOf('?')+1, url.length);
-let params = {
-    'year': null,
-    'month': null,
-};
-if (paramsStr.length > 0) {
-    let paramsList = paramsStr.split('&');
-    for (let p in paramsList) {
-        let par = paramsList[p].split('=');
-        if (par.length == 2) {
-            if (par[0] == 'year') {
-                params['year'] = par[1];
+        let monthE = document.querySelector('select#month');
+        let yearE = document.querySelector('select#year');
+
+        for (let i in yearE.options) {
+            if (yearE[i].value == year) {
+                yearE.selectedIndex = i;
             }
         }
+
+        key_order.shift();
+        const month = urlParams.get('month');
+        if (month != null) {
+            key_order.shift();
+            const day = urlParams.get('day');
+            const id = urlParams.get('id');
+
+            for (let i in monthE.options) {
+                if (monthE[i].value === months[months_n.indexOf(month)]) {
+                    monthE.selectedIndex = i;
+                }
+            }
+        }
+
+        fetch('/api/get-blog?' + urlParams)
+        .then(res => res.json())
+        .then(res => {
+            if (Array.isArray(res)) { // Day
+                if (res.length == 0) {
+                    return;
+                } else if (res.length == 1) { // Display Blog
+                    let blog_container = document.createElement('div');
+                    blog_container.classList.add('blog_container');
+                    let title = document.createElement('h1');
+                    title.innerHTML = res[0]['title'];
+                    let date = document.createElement('h2');
+                    date.innerHTML = res[0]['date'];
+                                       
+                    let main = document.querySelector('.main');
+                    blog_container.appendChild(title);
+                    blog_container.appendChild(date);
+                    let ps = res[0]['content'].split('\n');
+                    for (let p in ps) {
+                        let content = document.createElement('p');
+                        content.innerHTML = ps[p];
+                        blog_container.appendChild(content);
+                    }
+                    main.appendChild(blog_container);
+                } else {
+                    if (document.querySelector('.featured-blogs') == null) {
+                        let fb = document.createElement('div');
+                        fb.classList.add('featured-blogs');
+                        document.querySelector('.main').appendChild(fb);
+                    }
+                    console.log(res);  // Display All in that day
+                }
+            } else {  // Dictionary
+                let fb = document.querySelector('.featured-blogs');
+                if (fb == null) {
+                    fb = document.createElement('div');
+                    fb.classList.add('featured-blogs');
+                    document.querySelector('.main').appendChild(fb);
+                }
+                for (let key in res) {
+                    let monthHeader = document.createElement('h1');
+                    monthHeader.classList.add('monthHeader');
+                    if (key_order[0] == "month") {
+                        monthHeader.innerHTML = months[months_n.indexOf(key)];
+                        fb.appendChild(monthHeader);
+                        for (let day_key in res[key]) {
+                            for (let d in res[key][day_key]) {
+                                // <div class="fp-holder">
+                                //     <div class="fp">
+                                //         <img src="/blog_posts/Sailing to Maryland/1.jpg" alt="">
+                                //         <div class="blog_title"><h1>Sailing to Maryland</h1><h3>May 5th, 2024</h3></div>
+                                //     </div>
+                                // </div>
+                                let fpHolder = document.createElement('div');
+                                fpHolder.classList.add('fp-holder');
+                                let fp = document.createElement('div');
+                                fp.classList.add('fp');
+                                let bt = document.createElement('div');
+                                bt.classList.add('blog_title');
+                                let h1 = document.createElement('h1');
+                                h1.innerHTML = res[key][day_key][d]['title'];
+                                let h3 = document.createElement('h3');
+                                h3.innerHTML = res[key][day_key][d]['date'];
+                                bt.appendChild(h1);
+                                bt.appendChild(h3);
+                                fp.appendChild(bt);
+                                fpHolder.appendChild(fp);
+                                fb.appendChild(fpHolder);
+                                bt.addEventListener('click', function(e) {
+                                    let date = res[key][day_key][d]['date'].split('/');
+                                    location.href = "/blog?year=" + date[2] + '&month=' + date[0] + '&day=' + date[1] + '&id=' + d;
+                                    delete date;
+                                });
+                            }
+                        }
+                    } else {
+                        monthHeader.innerHTML = months[months_n.indexOf(month)] + ' ' + key;
+                        fb.appendChild(monthHeader);
+                        for (let d in res[key]) {
+                            // <div class="fp-holder">
+                            //     <div class="fp">
+                            //         <img src="/blog_posts/Sailing to Maryland/1.jpg" alt="">
+                            //         <div class="blog_title"><h1>Sailing to Maryland</h1><h3>May 5th, 2024</h3></div>
+                            //     </div>
+                            // </div>
+                            let fpHolder = document.createElement('div');
+                            fpHolder.classList.add('fp-holder');
+                            let fp = document.createElement('div');
+                            fp.classList.add('fp');
+                            let bt = document.createElement('div');
+                            bt.classList.add('blog_title');
+                            let h1 = document.createElement('h1');
+                            h1.innerHTML = res[key][d]['title'];
+                            let h3 = document.createElement('h3');
+                            h3.innerHTML = res[key][d]['date'];
+                            bt.appendChild(h1);
+                            bt.appendChild(h3);
+                            fp.appendChild(bt);
+                            fpHolder.appendChild(fp);
+                            fb.appendChild(fpHolder);
+                            bt.addEventListener('click', function(e) {
+                                let date = res[key][d]['date'].split('/');
+                                location.href = "/blog?year=" + date[2] + '&month=' + date[0] + '&day=' + date[1] + '&id=' + d;
+                                delete date;
+                            });
+                        }
+                    }
+                }
+            }
+        })
     }
 }
 
-console.log(params);
-
-
-
-
-
-
-
-
+load();
 
 
 
